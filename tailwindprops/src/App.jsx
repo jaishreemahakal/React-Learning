75f5fcedb1fd906ceb35456ff61e929c04d02ec8
Abhishek <abhishek23jangir@gmail.com>
2025-12-16 12:44:26 +0530

    learn about tailwind and props int jsx componets

---
+++ tailwindprops/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+//import card component
+import Card from './components/card'
+
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className='bg-green-500 text-black p-4 rounded-xl'>  Jai shree radhe shyam</h1>
+    <Card username="abhi"/>
+   
+    </>
+  )
+}
+
+export default App
+
+
